4248e5f1d080abbb6f9921a366fc10392c4a5e91
Alexsander Junior <alexsander.junior@luizalabs.com>
2023-06-14 18:50:04 -0300

    Adjusted celery.py

--- celery.py
+++ celery.py
@@ -1,14 +1,13 @@
 import os
-from celery import Celery, shared_task
+from celery import Celery
 
 # Configurações do Celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'personal_loan.configs.settings')
-app = Celery('personal_loan.common')
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app = Celery('personal_loan')
+app.config_from_object('django.conf:configs.settings', namespace='CELERY')
 app.autodiscover_tasks()
 
-
-@app.task(bind=True, ignore_result=True)
+@Celery.shared_task
 def processar_proposta(dados_proposta):
     # Lógica de processamento da proposta
     # Pode incluir envio de e-mails, cálculos, atualizações de estado, etc.
